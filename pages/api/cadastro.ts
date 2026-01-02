2968f80b6970329429b1dce4b982954760490c15
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-01-02 20:01:04 -0300

    concluida validação de usuario repetido

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -20,6 +20,12 @@ const endpointCadastro = async (
         if(!usuario.senha || usuario.senha.length < 4){
             return res.status(400).json({erro : 'senha invalida'});
         }
+
+        const usuarioComMesmoEmail = await UsuarioModel.findOne({email : usuario.email});
+        if(usuarioComMesmoEmail){
+            return res.status(400).json({erro : 'ja existe uma conta com esse email'});
+        }
+
         usuario.senha = md5(usuario.senha);
         await UsuarioModel.create(usuario);
         return res.status(200).json({msg: 'Usuario cadastrado com sucesso'});
