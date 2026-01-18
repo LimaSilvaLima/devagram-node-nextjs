c25159425feefc5fba800f534aefbf58d5dd6900
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-01-18 19:27:24 -0300

    get de usuario segunda revisao

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -4,8 +4,8 @@ import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 
 const usuarioEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-    return res.status(200).json({ msg: 'Usuario autenticado com sucesso' });
-    //res.status(200).json({ msg: 'Usuario autenticado com sucesso', userId: req.query.userId?.toString() || '' });
+    //return res.status(200).json({ msg: 'Usuario autenticado com sucesso' });
+    res.status(200).json({ msg: 'Usuario autenticado com sucesso', userId: req.query.userId?.toString() || '' });
 }
 
 export default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
