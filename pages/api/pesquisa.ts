f147e748b29a7d655dea32a7717cf24dd3012b44
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-01-23 11:20:24 -0300

    concluida persquisa de ususario por nome completo

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -6,9 +6,20 @@ import { UsuarioModel } from "../../models/UsuarioModel";
 import { createRouter } from "next-connect";
 import { upload, uploadImagemCosmic } from "../../services/uploadImagemCosmic";
 
-const pesquisaEndpoint = (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+const pesquisaEndpoint
+    = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>  | any []) => {
     try {
-        
+        if(req.method === 'GET') {
+            const {filtro} = req.query;
+            if(!filtro || filtro.length < 5) {
+                return res.status(400).json({msg: 'Favor informar o filtro da pesquisa com no minimo 5  caracteres'});
+            } 
+            const usuariosEncontrados = await UsuarioModel.find({
+                nome:  filtro                                                                                                                                                                                                                                                                                                                
+            });
+            return res.status(200).json(usuariosEncontrados);
+        }
+        return res.status(405).json({msg: 'Metodo informado nao e valido'});
     } catch (e) {
        console.log(e);
        return res.status(500).json({erro: "nao foi possível encontrar usuário" + e}); 
