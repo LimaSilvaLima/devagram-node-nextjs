1ebf765aacc6be065555304db91a1a017a629f39
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-01-23 15:08:25 -0300

    concluida pesquisa por id do usuario

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -10,17 +10,27 @@ const pesquisaEndpoint
     = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>  | any []) => {
     try {
         if(req.method === 'GET') {
-            const {filtro} = req.query;
-            if(!filtro || filtro.length < 5) {
-                return res.status(400).json({msg: 'Favor informar o filtro da pesquisa com no minimo 5  caracteres'});
-            } 
-            const usuariosEncontrados = await UsuarioModel.find({
-                $or :[{nome:  {$regex: filtro, $options: 'i'}},
-                {email:  {$regex: filtro, $options: 'i'}}]
-            });
+            if(req?.query.id) {
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query.id);
+                if(!usuarioEncontrado) {
+                    return res.status(400).json({msg: 'Usuario nao encontrado'});
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);  
+            } else {
+                const {filtro} = req.query;
+                if(!filtro || filtro.length < 5) {
+                     return res.status(400).json({msg: 'Favor informar o filtro da pesquisa com no minimo 5  caracteres'});
+              } 
+                const usuariosEncontrados = await UsuarioModel.find({
+                    $or :[{nome:  {$regex: filtro, $options: 'i'}},
+                    {email:  {$regex: filtro, $options: 'i'}}]
+                });
             return res.status(200).json(usuariosEncontrados);
         }
+        }
         return res.status(405).json({msg: 'Metodo informado nao e valido'});
+            
     } catch (e) {
        console.log(e);
        return res.status(500).json({erro: "nao foi possível encontrar usuário" + e}); 
